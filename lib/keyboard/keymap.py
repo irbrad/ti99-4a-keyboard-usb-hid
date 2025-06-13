c9427271dfc24b30aa70121e42d68f2f4550ccf6
Brad Angelcyk <brad@angelcyk.com>
2025-06-12 22:43:34 -0400

    updates pins to names in schematic to assist with debugging

--- lib/keyboard/keymap.py
+++ lib/keyboard/keymap.py
@@ -3,80 +3,80 @@ import digitalio
 from adafruit_hid.keycode import Keycode
 
 COLUMN_PINS = {
-  8: board.GP8,
-  9: board.GP9,
-  12: board.GP12,
-  13: board.GP13,
-  14: board.GP14,
-  15: board.GP15
+  "1Y1": board.GP8,
+  "1Y0": board.GP9,
+  "2Y0": board.GP12,
+  "2Y1": board.GP13,
+  "2Y2": board.GP14,
+  "2Y3": board.GP15
 }
 
 ROW_PINS = {
-  1: board.GP1,
-  2: board.GP2,
-  3: board.GP3,
-  4: board.GP4,
-  5: board.GP5,
-  6: board.GP6,
-  7: board.GP7,
-  10: board.GP10,
-  11: board.GP11
+  "INT5": board.GP1,
+  "INT6": board.GP2,
+  "INT8": board.GP3,
+  "INT4": board.GP4,
+  "INT3": board.GP5,
+  "P5": board.GP6,
+  "INT7": board.GP7,
+  "INT9": board.GP10,
+  "INT10": board.GP11
 }
 
 KEYMAP = {
-  (8, 5): ('/', Keycode.FORWARD_SLASH),
-  (8, 4): (';', Keycode.SEMICOLON),  # does not work
-  (8, 1): ('P', Keycode.P),
-  (8, 2): ('0', Keycode.ZERO),
-  (8, 11): ('Z', Keycode.Z),
-  (8, 3): ('A', Keycode.A),
-  (8, 10): ('Q', Keycode.Q), # doesn't work most times
-  (8, 7): ('1', Keycode.ONE), # does not work
+  ("1Y1", "INT3"): ('/', Keycode.FORWARD_SLASH),
+  ("1Y1", "INT4"): (';', Keycode.SEMICOLON),  # does not work
+  ("1Y1", "INT5"): ('P', Keycode.P),
+  ("1Y1", "INT6"): ('0', Keycode.ZERO),
+  ("1Y1", "INT10"): ('Z', Keycode.Z),
+  ("1Y1", "INT8"): ('A', Keycode.A),
+  ("1Y1", "INT9"): ('Q', Keycode.Q), # doesn't work most times
+  ("1Y1", "INT7"): ('1', Keycode.ONE), # does not work
 
-  (13, 5): ('.', Keycode.PERIOD),
-  (13, 4): ('L', Keycode.L), # does not work
-  (13, 1): ('O', Keycode.O),
-  (13, 2): ('9', Keycode.NINE),
-  (13, 11): ('X', Keycode.X),
-  (13, 3): ('S', Keycode.S),
-  (13, 10): ('W', Keycode.W),
-  (13, 7): ('2', Keycode.TWO),
+  ("2Y1", "INT3"): ('.', Keycode.PERIOD),
+  ("2Y1", "INT4"): ('L', Keycode.L), # does not work
+  ("2Y1", "INT5"): ('O', Keycode.O),
+  ("2Y1", "INT6"): ('9', Keycode.NINE),
+  ("2Y1", "INT10"): ('X', Keycode.X),
+  ("2Y1", "INT8"): ('S', Keycode.S),
+  ("2Y1", "INT9"): ('W', Keycode.W),
+  ("2Y1", "INT7"): ('2', Keycode.TWO),
 
-  (14, 5): (',', Keycode.COMMA),
-  (14, 4): ('K', Keycode.K), # does not work
-  (14, 1): ('I', Keycode.I),
-  (14, 2): ('8', Keycode.EIGHT),
-  (14, 11): ('C', Keycode.C),
-  (14, 3): ('D', Keycode.D),
-  (14, 10): ('E', Keycode.E),
-  (14, 7): ('3', Keycode.THREE),
+  ("2Y2", "INT3"): (',', Keycode.COMMA),
+  ("2Y2", "INT4"): ('K', Keycode.K), # does not work
+  ("2Y2", "INT5"): ('I', Keycode.I),
+  ("2Y2", "INT6"): ('8', Keycode.EIGHT),
+  ("2Y2", "INT10"): ('C', Keycode.C),
+  ("2Y2", "INT8"): ('D', Keycode.D),
+  ("2Y2", "INT9"): ('E', Keycode.E),
+  ("2Y2", "INT7"): ('3', Keycode.THREE),
 
-  (15, 5): ('M', Keycode.M),
-  (15, 4): ('J', Keycode.J), # does not work
-  (15, 1): ('U', Keycode.U),
-  (15, 2): ('7', Keycode.SEVEN),
-  (15, 11): ('V', Keycode.V),
-  (15, 3): ('F', Keycode.F),
-  (15, 10): ('R', Keycode.R),
-  (15, 7): ('4', Keycode.FOUR),
+  ("2Y3", "INT3"): ('M', Keycode.M),
+  ("2Y3", "INT4"): ('J', Keycode.J), # does not work
+  ("2Y3", "INT5"): ('U', Keycode.U),
+  ("2Y3", "INT6"): ('7', Keycode.SEVEN),
+  ("2Y3", "INT10"): ('V', Keycode.V),
+  ("2Y3", "INT8"): ('F', Keycode.F),
+  ("2Y3", "INT9"): ('R', Keycode.R),
+  ("2Y3", "INT7"): ('4', Keycode.FOUR),
 
-  (9, 5): ('N', Keycode.N),
-  (9, 4): ('H', Keycode.H), # does not work
-  (9, 1): ('Y', Keycode.Y),
-  (9, 2): ('6', Keycode.SIX),
-  (9, 11): ('B', Keycode.B),
-  (9, 3): ('G', Keycode.G),
-  (9, 10): ('T', Keycode.T),
-  (9, 7): ('5', Keycode.FIVE),
+  ("1Y0", "INT3"): ('N', Keycode.N),
+  ("1Y0", "INT4"): ('H', Keycode.H), # does not work
+  ("1Y0", "INT5"): ('Y', Keycode.Y),
+  ("1Y0", "INT6"): ('6', Keycode.SIX),
+  ("1Y0", "INT10"): ('B', Keycode.B),
+  ("1Y0", "INT8"): ('G', Keycode.G),
+  ("1Y0", "INT9"): ('T', Keycode.T),
+  ("1Y0", "INT7"): ('5', Keycode.FIVE),
 
-  (12, 5): ('=', Keycode.EQUALS), # does not work
-  (12, 4): ('SPACE', Keycode.SPACE), # does not work
-  (12, 1): ('ENTER', Keycode.ENTER),
-  (12, 2): ('UNUSED', Keycode.F24),
-  (12, 11): ('UNUSED', Keycode.F24),
-  (12, 3): ('SHIFT', None), # not yet implemented
-  (12, 10): ('CONTROL', Keycode.F24), # not yet implemented
-  (12, 7): ('FUNCTION', Keycode.F24), # not yet implemented
+  ("2Y0", "INT3"): ('=', Keycode.EQUALS), # does not work
+  ("2Y0", "INT4"): ('SPACE', Keycode.SPACE), # does not work
+  ("2Y0", "INT5"): ('ENTER', Keycode.ENTER),
+  ("2Y0", "INT6"): ('UNUSED', Keycode.F24),
+  ("2Y0", "INT10"): ('UNUSED', Keycode.F24),
+  ("2Y0", "INT8"): ('SHIFT', None), # not yet implemented
+  ("2Y0", "INT9"): ('CONTROL', Keycode.F24), # not yet implemented
+  ("2Y0", "INT7"): ('FUNCTION', Keycode.F24), # not yet implemented
 }
 
 def init_rows():
